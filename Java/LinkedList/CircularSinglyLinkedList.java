727ef5dfe440b145a827ed98dc8b7ab6d9f0b032
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-03-25 01:04:46 +0530

    Updated Linked List

--- Java/LinkedList/CircularSinglyLinkedList.java
+++ Java/LinkedList/CircularSinglyLinkedList.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
+//We keep track of the head node in circular singly linked list  
+
 class Node {
     int data;
     Node next;
 
     public Node(int data) {
-        this.data = data;
-        this.next = null;
+        this.data = data;                       //Assigns data to data variable of the node
+        this.next = null;                       //Makes the next pointer of that node as NULL
     }
 }
 
@@ -14,49 +16,49 @@ public class CircularSinglyLinkedList {
     Node head;
 
     public CircularSinglyLinkedList() {
-        head = null;
+        head = null;                            //To initialize the linked list head node
     }
 
     // Insert at the front of the list
     public void insertFront(int data) {
         Node newNode = new Node(data);
-        if (head == null) {
+        if (head == null) {                     //if list is empty, insert as first node(as for only one node, first and last node are the same)
             head = newNode;
-            head.next = head;
+            head.next = head;                   //connecting the next pointer of the node to itself as it is a circular linked list
         } else {
             newNode.next = head;
             Node current = head;
             while (current.next != head) {
                 current = current.next;
             }
-            current.next = newNode;
-            head = newNode;
+            current.next = newNode;               //making the node as first node
+            head = newNode;                        //pointing the head node to the new node
         }
     }
 
     // Insert at the end of the list
     public void insertRear(int data) {
         Node newNode = new Node(data);
-        if (head == null) {
+        if (head == null) {                         //if list is empty, insert as first node(as for only one node, first and last node are the same)
             head = newNode;
-            head.next = head;
+            head.next = head;                       //connecting the next pointer of the node to itself as it is a circular linked list
         } else {
             Node current = head;
-            while (current.next != head) {
+            while (current.next != head) {          //traversing till the last node
                 current = current.next;
             }
-            current.next = newNode;
-            newNode.next = head;
+            current.next = newNode;                 //to make it as the last node, storing the address of the first node in the next address
+            newNode.next = head;                    //pointing the head node to the next of new node
         }
     }
 
     // Insert at a given position in the list
     public void insertAtPos(int data, int position) {
         Node newNode = new Node(data);
-        if (head == null) {
+        if (head == null) {                         //if list is empty, insert as first node(as for only one node, first and last node are the same)
             head = newNode;
-            head.next = head;
-        } else if (position == 1) {
+            head.next = head;                       //connecting the next pointer of the node to itself as it is a circular linked list
+        } else if (position == 1) {                 //if position is 1 then insert as first node
             newNode.next = head;
             Node current = head;
             while (current.next != head) {
@@ -67,20 +69,20 @@ public class CircularSinglyLinkedList {
         } else {
             Node current = head;
             int i = 1;
-            while (i < position - 1 && current.next != head) {
+            while (i < position - 1 && current.next != head) {  //finding the node address where newnode is going to be added
                 current = current.next;
                 i++;
             }
-            newNode.next = current.next;
+            newNode.next = current.next;                        //inserting the newnode after prev pointer
             current.next = newNode;
         }
     }
 
     // Delete at the front of the list
     public void deleteFront() {
-        if (head == null) {
+        if (head == null) {                                                 //List empty condition
             return;
-        } else if (head.next == head) {
+        } else if (head.next == head) {                              //If there is only one node, then the next pointer of the node will point to itself
             head = null;
         } else {
             Node current = head;
@@ -94,9 +96,9 @@ public class CircularSinglyLinkedList {
 
     // Delete at the end of the list
     public void deleteRear() {
-        if (head == null) {
+        if (head == null) {                                         //List empty condition
             return;
-        } else if (head.next == head) {
+        } else if (head.next == head) {                              //If there is only one node, then the next pointer of the node will point to itself
             head = null;
         } else {
             Node current = head;
@@ -109,19 +111,19 @@ public class CircularSinglyLinkedList {
 
     // Method to display the contents of the list
     public void display() {
-        if (head == null) {
+        if (head == null) {                                                         //List empty condition
             System.out.println("List is empty!");
             return;
         }
         Node current = head;
-        do {
-            System.out.print(current.data + " ");
+        do {                                                                            //traverses all the nodes of the linked list.
+            System.out.print(current.data + " ");                                       //prints the data of the nodes
             current = current.next;
         } while (current != head);
         System.out.println();
     }
 
-    //main method
+ //Menu based operation
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         CircularSinglyLinkedList sll = new CircularSinglyLinkedList();
